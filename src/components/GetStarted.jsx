e1ddd564f3c916e0f13b562d8e82adcb548b412a
myh <mkonji23@naver.com>
2022-09-24 13:10:14 +0900

    초기화

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,17 @@
+import styles from '../style'
+import { arrowUp } from '../assets'
+
+export const GetStarted = () => {
+  return (
+      <div className={`${styles.flexCenter} w-[140px] 
+      h-[140px] rounded-full
+      bg-blue-gradient p-[2px] cursor-pointer`}>
+        <div className={`${styles.flexCenter}
+        flex-col bg-primary w-[100%] h-[100%]
+        rounded-full`}>
+          <div className={`${styles.}`}
+        </div>
+      </div>
+    )
+
+}
