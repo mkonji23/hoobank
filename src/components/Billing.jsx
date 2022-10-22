6aa1444cf2ebd42e7d680cf944db222afd2dc43e
myh <mkonji23@naver.com>
2022-10-22 14:32:32 +0900

    Testimonials Complete

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -6,13 +6,13 @@ export const Billing = () =>(
   <section id='product' className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt='billing'
-        classNmae='w-[100%] h-[100%] relative z-[5]'>
+        className='w-[100%] h-[100%] relative z-[5]'>
       </img>
 
-      <div className='aboslute z-[3] -left-1/2
+      <div className='absolute z-[3] -left-1/2
       top-0 w-[50%] h-[50%] rounded-full
       white__gradient'/>
-      <div className='aboslute z-[0] -left-1/2
+      <div className='absolute z-[0] -left-1/2
       bottom-0 w-[50%] h-[50%] rounded-full
       pink__gradient'/>
     </div>
